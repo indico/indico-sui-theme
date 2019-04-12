54953d42d96266d617fb3b2989fcc6f8b2350d3d
Adrian Moennich <adrian.moennich@cern.ch>
2019-04-12 14:20:52 +0200

    Fix eslint warnings

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
-const util = require('util');
-const { series, src, dest, symlink } = require('gulp');
+const {series, src, dest, symlink} = require('gulp');
 const watch = require('gulp-watch');
 const {spawn} = require('child_process');
 const del = require('del');
@@ -7,10 +6,10 @@ const del = require('del');
 // from https://stackoverflow.com/a/50396702/682095
 async function pipe(tap, sink) {
     return new Promise((resolve, reject) => {
-        tap.pipe(sink, {end: false})
-        tap.on("end", resolve)
-        tap.on("error", reject)
-    })
+        tap.pipe(sink, {end: false});
+        tap.on('end', resolve);
+        tap.on('error', reject);
+    });
 }
 
 function spawnPromise(...args) {
@@ -18,7 +17,7 @@ function spawnPromise(...args) {
         const proc = spawn(...args);
         proc.on('close', resolve);
     });
-};
+}
 
 function clean() {
     return del(['build/**/*']);
@@ -50,7 +49,7 @@ async function setup() {
         src('build/src/themes/default/modules/{checkbox,dropdown,accordion}.overrides'),
         dest('build/themes/indico/modules/')
     );
-     await pipe(
+    await pipe(
         src('semantic.json'),
         symlink('build/')
     );
@@ -69,7 +68,7 @@ async function _watch() {
 const build = series(setup, npm, _build);
 
 exports.rebuild = series(clean, build);
-exports.build = build
+exports.build = build;
 exports.watch = series(setup, npm, _watch);
 exports.clean = clean;
 exports.default = build;
